2080d0bf4a5b06c6f8140f2e10a39b29bc1a6705
htyo <hontian921004@gmail.com>
2018-04-14 17:12:20 +0800

    Initial commit

---
+++ app/src/main/java/com/edu/bag/bagedu/MainActivity.java
@@ -0,0 +1,13 @@
+package com.edu.bag.bagedu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
